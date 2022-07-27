6a34693da161c3d8a65a46c64d47cd0e7fa7c7b6
sandhya <sandhyarani@outliers.tech>
2022-07-27 12:16:49 +0530

    state city area functionality was done

--- argonapp/models.py
+++ argonapp/models.py
@@ -5,6 +5,33 @@ from django.db import models
 class Country(models.Model):
     id = models.AutoField(primary_key=True)
     country = models.CharField(max_length=200)
-    status = models.IntegerField()
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class State(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.SET_NULL, related_name='state')
+    state = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class City(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.SET_NULL, related_name='city')
+    state = models.ForeignKey(State, null=True, blank=True, on_delete=models.SET_NULL, related_name='cities')
+    city = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Area(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.SET_NULL, related_name='area_country')
+    state = models.ForeignKey(State, null=True, blank=True, on_delete=models.SET_NULL, related_name='area_state')
+    city = models.ForeignKey(City, null=True, blank=True, on_delete=models.SET_NULL, related_name='area_city')
+    area = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- argonapp/views.py
+++ argonapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render , redirect
 from django.shortcuts import HttpResponse
 from django.core.mail import send_mail
 from  django.contrib.auth.models import User,auth
-from .models import Country
+from .models import Country, State, City, Area
 from django.http import HttpResponse, JsonResponse
 
 from django.contrib.auth import login,authenticate
@@ -92,6 +92,154 @@ def country_delete(request):
             country = Country.objects.get(id=country_id)
             country.delete()
             return redirect("country_view")
+
+        except Exception as e:
+            return JsonResponse({'msg': str(e)}, status=400)
+
+def state_view(request):
+    state_id = request.GET.get('id')
+    state_one = []
+
+    if state_id:
+        state_one = State.objects.get(id=state_id)
+    state = State.objects.all().order_by('state')
+    return render(request, "state/state_list.html",
+                  {'state': state, 'state_one': state_one})
+
+def state_add(request):
+    state_id = request.GET.get('id')
+    state_qs = []
+    if state_id:
+        state_qs = State.objects.get(id=int(state_id))
+    countries = Country.objects.all().order_by('country')
+    return render(request, 'state/state_add.html', {'state_qs': state_qs, 'countries': countries})
+
+
+def state_entry(request):
+    id = request.POST.get('id')
+    country = request.POST.get('country')
+    state = request.POST.get('state')
+    status = request.POST.get('status')
+    country_one = Country.objects.get(id=country)
+
+    if id:
+        state_qs = State.objects.get(id=id)
+        state = State(id=state_qs.id, country=country_one, state=state, status=status)
+
+    else:
+        state = State(country=country_one, state=state, status=status)
+
+    state.save()
+    return redirect("state_view")
+
+def state_delete(request):
+    state_id = request.GET.get('id')
+    if state_id:
+        try:
+            state = State.objects.get(id=state_id)
+            state.delete()
+            return redirect("state_view")
+
+        except Exception as e:
+            return JsonResponse({'msg': str(e)}, status=400)
+
+def city_view(request):
+    city_id = request.GET.get('id')
+    city_one = []
+    if city_id:
+        city_one = City.objects.get(id=city_id)
+    city = City.objects.all().order_by('city')
+    return render(request, "city/city_list.html",
+                  {'city': city, 'city_one': city_one})
+
+def city_add(request):
+    city_id = request.GET.get('id')
+    city_qs = []
+    if city_id:
+        city_qs = City.objects.get(id=int(city_id))
+    countries = Country.objects.all().order_by('country')
+    states = State.objects.all().order_by('state')
+    return render(request, 'city/city_add.html', {'city_qs': city_qs, 'countries': countries, 'states': states})
+
+
+def city_entry(request):
+    id = request.POST.get('id')
+    country = request.POST.get('country')
+    state = request.POST.get('state')
+    city = request.POST.get('city')
+    status = request.POST.get('status')
+    country_one = Country.objects.get(id=country)
+    state_one = State.objects.get(id=state)
+
+    if id:
+        city_qs = City.objects.get(id=id)
+        city = City(id=city_qs.id, country=country_one, state=state_one, status=status, city=city)
+
+    else:
+        city = City(country=country_one, state=state_one, status=status, city=city)
+
+    city.save()
+    return redirect("city_view")
+
+def city_delete(request):
+    city_id = request.GET.get('id')
+    if city_id:
+        try:
+            state = City.objects.get(id=city_id)
+            state.delete()
+            return redirect("city_view")
+
+        except Exception as e:
+            return JsonResponse({'msg': str(e)}, status=400)
+
+def area_view(request):
+    area_id = request.GET.get('id')
+    area_one = []
+    if area_id:
+        area_one = Area.objects.get(id=area_id)
+    area = Area.objects.all().order_by('city')
+    return render(request, "area/area_list.html",
+                  {'area': area, 'area_one': area_one})
+
+def area_add(request):
+    area_id = request.GET.get('id')
+    area_qs = []
+    if area_id:
+        area_qs = Area.objects.get(id=int(area_id))
+    countries = Country.objects.all().order_by('country')
+    states = State.objects.all().order_by('state')
+    cities = City.objects.all().order_by('city')
+    return render(request, 'area/area_add.html', {'area_qs': area_qs, 'countries': countries, 'states': states, 'cities':cities})
+
+
+def area_entry(request):
+    id = request.POST.get('id')
+    country = request.POST.get('country')
+    state = request.POST.get('state')
+    city = request.POST.get('city')
+    area = request.POST.get('area')
+    status = request.POST.get('status')
+    country_one = Country.objects.get(id=country)
+    state_one = State.objects.get(id=state)
+    city_one = City.objects.get(id=city)
+
+    if id:
+        area_qs = Area.objects.get(id=id)
+        area = Area(id=area_qs.id, country=country_one, state=state_one, city=city_one, area=area, status=status)
+
+    else:
+        area = Area(country=country_one, state=state_one, area=area, city=city_one, status=status)
+
+    area.save()
+    return redirect("area_view")
+
+def area_delete(request):
+    area_id = request.GET.get('id')
+    if area_id:
+        try:
+            area = Area.objects.get(id=area_id)
+            area.delete()
+            return redirect("area_view")
 
         except Exception as e:
             return JsonResponse({'msg': str(e)}, status=400)
